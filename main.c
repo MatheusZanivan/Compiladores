9ffccc93a5f05771f95bc18c03c98176877163d3
Matheus Zanivan <zanivanandrade@gmail.com>
2023-03-27 19:44:11 -0300

    recebe o arquivo que será interpretado

--- main.c
+++ main.c
@@ -49,15 +49,45 @@ Token* tokenizar(char* s, int* tamanho) {
     return tokens;
 }
 
-int main() {
-    int tamanho;
-    Token* tokens = tokenizar("+-*/=<>==", &tamanho);
-    printf("Tokens: ");
-    for (int i = 0; i < tamanho; i++) {
-        printf("%c ", tokens[i].operador);
-    }
-    printf("\n");
-    free(tokens);
-    return 0;
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Uso: %s <arquivo>\n", argv[0]);
+		return 1;
+	}
+	
+	FILE* fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		printf("Erro ao abrir arquivo %s\n", argv[1]);
+		return 1;
+	}
+	
+	// determina o tamanho do arquivo
+	fseek(fp, 0, SEEK_END);
+	long size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	
+	// aloca espaço para a string que irá armazenar o conteúdo do arquivo
+	char* s = (char*) malloc(size + 1);
+	if (s == NULL) {
+		printf("Erro de alocação de memória\n");
+		return 1;
+	}
+	
+	// lê o conteúdo do arquivo para a string s
+	fread(s, 1, size, fp);
+	s[size] = '\0';
+	fclose(fp);
+	
+	int tamanho;
+	Token* tokens = tokenizar(s, &tamanho);
+	printf("Tokens: ");
+	for (int i = 0; i < tamanho; i++) {
+		printf("%c ", tokens[i].operador);
+	}
+	printf("\n");
+	free(tokens);
+	free(s);
+	return 0;
 }
+
 
